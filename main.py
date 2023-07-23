befedf9fcb4161ecb7342c295265e0b04083e821
pakitowka <whitesaint12@gmail.com>
2023-07-23 03:00:38 +0200

    generating .pdf

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about bill, such as
@@ -21,6 +24,7 @@ class Flatmate:
 
     def pays(self, bill, flatmate2):
         """Counts amount to pay for flatmate."""
+
         weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
         to_pay = weight * bill.amount
         return to_pay
@@ -34,16 +38,35 @@ class PdfReport:
     """
 
     def __init__(self,filename):
-        self.filename = filename
+        self.filename = f"reports/{filename}.pdf"
 
     def generate(self, flatmate1, flatmate2, bill):
         """Generates a .pdf report"""
-        pass
+
+        pdf = FPDF(orientation="P", unit='pt', format="A4")
+        pdf.add_page()
+
+        # Insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+
+        # Insert period label and value
+        pdf.cell(w=100, h=40, txt="Period:", border=1)
+        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+
+        # First flatmate data
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill=bill, flatmate2=flatmate2)), border=1, ln=1)
+
+        pdf.output(self.filename)
 
 
-the_bill = Bill(amount=120, period="July 2023")
+the_bill = Bill(amount=120, period="June 2023")
 john = Flatmate(name="John", days_in_house=20)
 marry = Flatmate(name="Marry", days_in_house=25)
 
 print(f"{john.name} pays: ", john.pays(bill=the_bill, flatmate2=marry))
 print(f"{marry.name} pays: ", marry.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename="Report1")
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
